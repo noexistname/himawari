259725781205284ab7b5389519486a0d08b33681
noexistname <167598494+noexistname@users.noreply.github.com>
2025-02-14 21:19:23 +0800

    Update himawari.sh

--- himawari.sh
+++ himawari.sh
@@ -1,19 +1,6 @@
 # 更新系统并安装必需的软件
 echo "Updating system packages and installing dependencies..."
-apt-get installsudo -y
-sudo apt-get update -y && sudo apt upgrade -y
-
-# 配置自定义 Bash 提示符和别名
-echo "Configuring custom PS1 and aliases..."
-echo "PS1='[\e[32;40m\e[1m\u\e[32;40m\e[1m@\e[35;40m\e[1m\h\e[0m \e[34;40m\e[1m\W\e[0m]\$ '" >> /etc/bash.bashrc
-echo "alias ls='ls --color=auto'" >> /etc/bash.bashrc
-echo "alias ll='ls --color=auto -l'" >> /etc/bash.bashrc
-echo "alias egrep='egrep --color=auto'" >> /etc/bash.bashrc
-echo "alias fgrep='fgrep --color=auto'" >> /etc/bash.bashrc
-echo "alias grep='grep --color=auto'" >> /etc/bash.bashrc
-
-# 使 Bash 配置立即生效
-source /etc/bash.bashrc
+apt-get install sudo -y
 
 # 配置 BBR TCP 拥塞控制
 echo "Configuring BBR TCP Congestion Control..."
@@ -23,7 +10,7 @@ sudo sysctl -p
 
 # 安装并配置防火墙（ufw）
 echo "Installing UFW and configuring firewall..."
-sudo apt-get install -y ufw
+sudo apt-get install -y
 sudo ufw enable -y
 
 # 提示用户输入 SSH 端口号
@@ -45,6 +32,18 @@ sudo sed -i "s/^#Port 22/Port $ssh_port/" /etc/ssh/sshd_config
 # 重启 SSH 服务
 echo "Restarting SSH service..."
 sudo systemctl restart sshd
+
+# 配置自定义 Bash 提示符和别名
+echo "Configuring custom PS1 and aliases..."
+echo "PS1='[\e[32;40m\e[1m\u\e[32;40m\e[1m@\e[35;40m\e[1m\h\e[0m \e[34;40m\e[1m\W\e[0m]\$ '" >> /etc/bash.bashrc
+echo "alias ls='ls --color=auto'" >> /etc/bash.bashrc
+echo "alias ll='ls --color=auto -l'" >> /etc/bash.bashrc
+echo "alias egrep='egrep --color=auto'" >> /etc/bash.bashrc
+echo "alias fgrep='fgrep --color=auto'" >> /etc/bash.bashrc
+echo "alias grep='grep --color=auto'" >> /etc/bash.bashrc
+
+# 使 Bash 配置立即生效
+source /etc/bash.bashrc
 
 echo "All tasks completed successfully!"
 
